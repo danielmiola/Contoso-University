b6b0f1eca6e3668144028592a7654a965853e203
danielmiola <daniel.miola@hotmail.com>
2015-10-22 10:06:13 -0200

    Ended "Create more complex data" and "Reading related data"

--- ContosoUniversity/Migrations/201510221105162_ComplexDataModel].Designer.cs
+++ ContosoUniversity/Migrations/201510221105162_ComplexDataModel].Designer.cs
@@ -7,13 +7,13 @@ namespace ContosoUniversity.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class InitialCreate : IMigrationMetadata
+    public sealed partial class ComplexDataModel : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialCreate));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(ComplexDataModel));
         
         string IMigrationMetadata.Id
         {
-            get { return "201510201448178_InitialCreate"; }
+            get { return "201510221105162_ComplexDataModel]"; }
         }
         
         string IMigrationMetadata.Source
